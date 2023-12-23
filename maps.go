3355e2a5525ef2ad47d4fd6433d1bd0bb63360e0
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2023-12-23 13:41:01 +0500

    String helpers

--- maps.go
+++ maps.go
@@ -1,23 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println(getAverage([]float64{12, 21, 21, 31, 23, 123, 213, 123, 21, 321, 321, 321, 312}))
-	fmt.Println(get())
-}
 
-func getAverage(arr []float64) (avg float64, sum float64) {
-	sum = 0.0
+	str := "a-b-c-d-f"
+	var arr []string
+	arr = strings.Split(str, "-")
 
-	for _, value := range arr {
-		sum += value
-	}
+	fmt.Println(arr[2:4])
 
-	avg = sum / float64(len(arr))
-	return
-}
-
-func get() (int, int) {
-	return 3, 5
 }
